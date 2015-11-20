d3cd2803eda8204a1bccf48ae98b9a52168d5915
TheNeeraj <neeraj@repup.co>
2015-11-20 13:44:51 +0530

    comp half

---
+++ app/components/hotel/competitiveAnalysis/CatagoryScoresData.js
@@ -0,0 +1,17 @@
+export default{
+	catagoryScoresData: [
+    {
+      hotelName:'Galaxy',
+      catagories:[
+        {
+          catagoryName:'Washrooms',
+          reviews:'6',
+          score:'42',
+          positiveReviews:'25',
+          neutralReviews:'50',
+          negativeReviews:'25'
+        }
+      ]
+    }
+  ]
+}
